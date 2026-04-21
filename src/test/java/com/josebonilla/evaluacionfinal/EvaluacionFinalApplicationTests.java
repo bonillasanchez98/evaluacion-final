dcff8d1c4354dddecd37cafd01423c8ee8fde006
Jose Bonilla Sanchez <bonillasanchez98@gmail.com>
2026-04-20 20:26:36 -0400

    First commit

---
+++ src/test/java/com/josebonilla/evaluacionfinal/EvaluacionFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josebonilla.evaluacionfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
